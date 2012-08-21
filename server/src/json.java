771c3573ddfd60f8005d1a0d328c96306033f178
np3228k@gmail.com <np3228k@gmail.com@b7bd332d-3afc-6883-9f51-6802bfb3a3cc>
2012-08-21 13:48:08 +0000

    json

---
+++ server/src/json.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+
+public class json {
+
+	public static void main(String[] args) {
+		Socket socket = null;
+
+		try {
+			socket = new Socket("14.63.223.201", 8124);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			PrintWriter writer = new PrintWriter(socket.getOutputStream());
+			Scanner a = new Scanner(System.in);
+			String str1 = a.next();
+			writer.println(str1);
+			writer.flush();
+			String str = reader.readLine();
+			JSONObject js = new JSONObject();
+			String i = js.getString(str);
+			System.out.println(i);
+
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		finally {
+			try {
+				socket.close();
+			}
+			catch(Exception e) {
+				
+			}
+		}
+	}
+}
